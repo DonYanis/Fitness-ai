347bf4fc60804b85683f1513ece98c989e4d86ae
DonYanis <beldjilaliyanis@gmail.com>
2023-07-21 12:09:28 +0100

    final commit

--- api/views.py
+++ api/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from api.serializers import UserSerializer
+import ast
 
 from api.ai.ai_controller import generate_ai_data
-from api.utils import update_user
+from api.utils import update_user, hash_password, verify_password
 
 @api_view(['POST'])
 def home(request):
@@ -24,29 +25,65 @@ def home(request):
     
     return Response({'status' : 'success','data' : result})
 
-@api_view(['GET'])
-def getUsers(request):
+@api_view(['GET','POST'])
+def usersController(request):
 
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET' : 
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST' : 
+        try :
+            user = User.objects.get(username = request.data['username'])
+            serializer = UserSerializer(user)
+            data = serializer.data
+
+            res = {
+                'status' : 'success',
+                'data' : {
+                    'health': data['health'],
+                    'food': data['food'],
+                    'training': data['training'],
+                    'program': ast.literal_eval(data['program']),
+                    'eat': ast.literal_eval(data['eat']),
+                    'avoid': ast.literal_eval(data['avoid']),
+                    'advice': ast.literal_eval(data['advice']),
+                    'macros': {
+                        'calories': int(data['calories']),
+                        'protein': int(data['protein']),
+                        'fat': int(data['fat']),
+                        'carbs': int(data['carbs']),
+                        'fibers': int(data['fibers'])
+                        }
+                }
+            }
+            return Response(res)
+            
+        except Exception as e:
+            return Response({'status' : 'fail','message' : 'ERROR !'},status=status.HTTP_400_BAD_REQUEST)
+    
+
 
 @api_view(['POST'])
 def login(request):    
-    created = False
     try :
-        data  = request.data
+        data = request.data
         if User.objects.filter(username = data['username']).exists() :
-            if not User.objects.filter(username= data['username'], password=data['password']).exists() :
-                return Response({'status' : 'fail','message' : 'change username or password'},status=status.HTTP_400_BAD_REQUEST)
+            #verify the password : 
+            user = User.objects.get(username=data['username'])
+            is_password_correct = verify_password(user.password, data['password'])
+            if is_password_correct :
+                return Response({'status' : 'success', 'username':data['username'], 'hasData': user.has_data})
+            return Response({'status' : 'fail','message' : 'change username or password'},status=status.HTTP_400_BAD_REQUEST)
+
         else :
-            created = True
-            User.objects.create(username=data['username'], password=data['password'])
+            #hash the password : 
+            hashed_password = hash_password(data['password'])
+            User.objects.create(username=data['username'], password=hashed_password)
+            return Response({'status' : 'success','hasData' : False, 'username':data['username']})
 
     except Exception as e:
         print(e)
         return Response({'status' : 'fail','message' : 'user not logged in'},status=status.HTTP_400_BAD_REQUEST)
-    if created :
-        return Response({'status' : 'success','message' : 'created', 'username':data['username']})
-    else :
-        return Response({'status' : 'success','message' : 'logged in', 'username':data['username']})
+    
